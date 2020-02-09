4fa38f94508b90c0ce81d4a68ee9f43a5924fbf5
MaksymBohdan <m.bohdan@globit.com>
2020-02-09 13:39:15 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -3,18 +3,16 @@ import './App.css';
 import properties$ from './mock';
 
 class App extends Component {
-
   componentDidMount() {
-    properties$.subscribe((data) => {
+    properties$.subscribe(data => {
       console.log('data', data);
-    })
+    });
   }
 
   render() {
-    return (
-      <div className="app">
-      </div>
-    );
+    console.log(this.props);
+
+    return <div className="app"></div>;
   }
 }
 
