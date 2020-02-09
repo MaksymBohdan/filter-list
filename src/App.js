8c7ac31b2480212aebd39f76c12f84b04d300e3b
MaksymBohdan <maxymbohdan@gmail.com>
2020-02-09 14:19:29 +0200

    test

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import properties$ from './mock';
 class App extends Component {
   componentDidMount() {
     properties$.subscribe(data => {
+      const a = 'adad';
       // console.log('data', data);
     });
   }
