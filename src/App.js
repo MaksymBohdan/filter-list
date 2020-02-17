3058396c81d98a6e446d553bfb361234a0cd6a73
MaksymBohdan <maxymbohdan@gmail.com>
2020-02-17 22:06:22 +0200

    feat/pagination

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Table from './modules/Table/TableContainer';
 import Header from './modules/Header/HeaderContainer';
+import Pagination from './modules/Pagination/PaginationContainer';
 
 const App = () => (
   <>
     <Header />
     <Table />
+    <Pagination />
   </>
 );
 
